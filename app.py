809858a3fb29bb319871d3ca6a3a3a71781cb957
Antti Laaksonen <ahslaaks@cs.helsinki.fi>
2024-11-07 19:43:16 +0200

    Remove db import

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import abort, make_response, redirect, render_template, request, session
-import config, db, forum, users
+import config, forum, users
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
